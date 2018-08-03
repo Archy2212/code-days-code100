646ff2250458226bd741ac92f970a28aea019d24
Archy Mathur <archy2212@gmail.com>
2018-08-03 22:57:01 +0530

    day 7

---
+++ python/math.py
@@ -0,0 +1,33 @@
+import math
+
+#no need to declare datatype
+print(2)
+print(3.6)
+print(-5.9)
+
+#basic operation on numbers
+print(2+7)
+print(22/7)
+print(3/3+3*9)
+
+#explicit conversion
+my_fav=22
+print(str(my_fav) +" is my fav no.")
+
+#absolute
+print(abs(-10))
+
+#exponential
+print(pow(4,2))
+
+#round off
+print(round(5.5))
+print(round(2.3))
+print(round(8.9))
+
+#using math function
+
+print(math.sqrt(4))
+print(math.floor(3.6))
+print(math.ceil(3.6))
+print(math.pi)
---
+++ python/starting.py
@@ -0,0 +1,13 @@
+#variable declaration
+name="Archy Mathur"
+print("Hello " +name)
+
+#how to find index
+print(name.index("h"))
+
+#replace a substring
+print(name.replace("Archy","Suryansh"))
+
+#input from user
+name1=input("enter your name ")
+print("hello "+name1+"! hope you are enjoing here.")
