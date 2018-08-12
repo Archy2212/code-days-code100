cf9a6392aa259050dc4c0d0fde5cce58c08ef47f
Archy Mathur <archy2212@gmail.com>
2018-08-12 22:36:48 +0530

    day 15

--- node JS/poly.js
+++ node JS/poly.js
@@ -1,36 +1,11 @@
 var Polynomial = require('polynomial');
 const readline = require('readline');
 
-const r2= readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-var eq=function(){
-r2.question('enter your eq:',(answer2)=>{
-r2.close();
-  console.log( (`${answer2}`));
-})
-});
+
+
+var p=function(x){
+console.log(Polynomial(poly));
 }
-
-eq();
-
-/*
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-r1.question('enter the equation? ', (answer2) => {
-
-  console.log(` valuable feedback: ${answer2}`);
-
-  rl1.close();
-});
-*/
-/*var p=function(x){
-return (Polynomial(poly);
-}
-console.log("enter a polynomial-");
+console.log("Polynomial is:");
 var poly = new Polynomial("x^3-5x+1");
-p(poly);*/
+p(poly);
