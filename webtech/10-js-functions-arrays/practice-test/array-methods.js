d147f6a77f69282ea47d7fa4b0ffda3545533f34
Archy Mathur <archy2212@gmail.com>
2018-08-31 22:17:43 +0530

    practice

--- webtech/10-js-functions-arrays/practice-test/array-methods.js
+++ webtech/10-js-functions-arrays/practice-test/array-methods.js
@@ -1,32 +1,35 @@
 // Create and initialize an Array
-
+let arr = [2,4,1,5,6];
 
 // shift one element
-
+console.log(arr.shift());
 
 // print remaining array
-
+console.log(arr);
 
 // unshift a number
-
+console.log(arr.unshift(8));
 
 //print array
-
+console.log(arr);
 
 // pop an element from array
-
+console.log(arr.pop());
 
 //print array
-
+console.log(arr);
 
 // push an element
+console.log(arr.push(9));
 
 // print element
+console.log(arr);
 
 // use splice to change value at a specific position
+console.log(arr.splice(3,1,10,44));
 
 // print the array
-
+console.log(arr);
 
 
 
--- webtech/10-js-functions-arrays/practice-test/array-practice.js
+++ webtech/10-js-functions-arrays/practice-test/array-practice.js
@@ -1,12 +1,18 @@
 // Let's initialize an array consisting the names of family members
-
+let family = ["archy","suryansh","anshika","kartik","navni"];
 // print all the values of array
+console.log(family);
 
 // print value from a specific index , from index one
+console.log(family[2]);
 
 //print length of array
+console.log(family.length);
 
 //print last element in array
+console.log(family[family.length-1]);
 
 //print all the values of array one by one
-
+family.forEach(function(x){
+    console.log(x);
+});
