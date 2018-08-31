02f0e0eaa12127362bcc4bf29067dbbc8c3671df
Archy Mathur <archy2212@gmail.com>
2018-08-31 21:46:46 +0530

    day 30

---
+++ webtech/10-js-functions-arrays/practice-test/array-methods.js
@@ -0,0 +1,36 @@
+// Create and initialize an Array
+
+
+// shift one element
+
+
+// print remaining array
+
+
+// unshift a number
+
+
+//print array
+
+
+// pop an element from array
+
+
+//print array
+
+
+// push an element
+
+// print element
+
+// use splice to change value at a specific position
+
+// print the array
+
+
+
+
+
+
+
+
---
+++ webtech/10-js-functions-arrays/practice-test/array-practice.js
@@ -0,0 +1,12 @@
+// Let's initialize an array consisting the names of family members
+
+// print all the values of array
+
+// print value from a specific index , from index one
+
+//print length of array
+
+//print last element in array
+
+//print all the values of array one by one
+
