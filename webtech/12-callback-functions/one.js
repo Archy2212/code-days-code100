8ed4c41849e08922b17e738a206053192885b693
Archy Mathur <archy2212@gmail.com>
2018-09-07 22:02:34 +0530

    edited

--- webtech/12-callback-functions/one.js
+++ webtech/12-callback-functions/one.js
@@ -1,7 +1,7 @@
 const todo =[];
-todo.push("Make youtube video");
-todo.push("Push JS code on Github");
-todo.push("Next ML project to generate music");
+todo.push("Make youtube video!");
+todo.push("Push JS code on Github!");
+todo.push("Next ML project to generate music!");
 
 todo.forEach(function(todoItem, index) {
     console.log(`Todo item at ${index} is ${todoItem}`);
