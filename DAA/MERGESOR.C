17cb4a617ed26f9b2a5e4056a0882ba08e39cae0
Archy Mathur <archy2212@gmail.com>
2018-09-08 23:39:23 +0530

    day 40

---
+++ DAA/MERGESOR.C
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+void merge_sorted_subarray(int a[],int i1,int j1,int i2,int j2);
+void merge_sort(int a[],int beg,int end)
+{
+ int mid;
+ if(beg<end)
+ {
+  mid=(beg+end)/2;
+  merge_sort(a,beg,mid);
+  merge_sort(a,mid+1,end);
+  merge_sorted_subarray(a,beg,mid,mid+1,end);
+ }
+}
+
+void merge_sorted_subarray(int a[],int i1,int j1,int i2,int j2)
+{
+ int k=0,i=i1,j=i2;
+ int temp[100];
+ while(i<=j1&&j<=j2)
+ {
+  if(a[i]<a[j])
+   temp[k++]=a[i++];
+  else
+   temp[k++]=a[j++];
+ }
+ while(i<=j1)
+  temp[k++]=a[i++];
+ while(j<=j2)
+  temp[k++]=a[j++];
+ for(i=i1,j=0;i<=j2;i++,j++)
+  a[i]=temp[j];
+}
+void main()
+{
+ int n,i,a[1000];
+ clrscr();
+ printf("enter number of elements:\n");
+ scanf("%d",&n);
+ printf("enter the elements:\n");
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+ merge_sort(a,0,n-1);
+ printf("the sorted elements are:");
+ for(i=0;i<n;i++)
+ printf("%d\t",a[i]);
+ getch();
+}
