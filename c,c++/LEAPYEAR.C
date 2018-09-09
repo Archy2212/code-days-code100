933bc5972bebab2793cff526566957587dafb15e
Archy Mathur <archy2212@gmail.com>
2018-09-09 23:42:25 +0530

    day 41 code 1

---
+++ c,c++/LEAPYEAR.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int year;
+ printf("enter the year");
+ scanf("%d",&year);
+ if((year%4==0&&year%100!=0)||(year%400==0))
+ {
+  printf("%d is leap year",year);
+ }
+ else
+ {
+  printf("not leap year");
+ }
+ }
