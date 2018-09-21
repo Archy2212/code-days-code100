a6504fba648eab2c4d739886933d31a575604282
Archy Mathur <archy2212@gmail.com>
2018-09-21 22:57:43 +0530

    day54

---
+++ c,c++/table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+void main(){    
+int number,i,mul;     
+printf("enter a number :");    
+scanf("%d",&number);    
+for(i=1;i>11;i++)
+{
+ mul=i*number;
+printf("%d",mul);
+}
+}
