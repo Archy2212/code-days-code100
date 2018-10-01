633a2c1b940be264f4748db9529e75cb47fdc574
Archy Mathur <archy2212@gmail.com>
2018-10-01 23:02:07 +0530

    delete

--- webtech/php-lectures/day-5/include/menu.php
+++
@@ -1,4 +0,0 @@
-<a href="http://kiet.edu">Home</a> - 
-<a href="http://kiet.edu/BTECH">BTECH</a> - 
-<a href="http://kiet.edu/MTECH">MTECH</a> - 
-<a href="http://kiet.edu/MCA">MCA</a> <br />
--- webtech/php-lectures/day-5/include/test-require.php
+++
@@ -1,8 +0,0 @@
-<html>
-   <body>
-   
-      <?php require("x-menu.php"); ?>
-      <p>This is an example to show how to include PHP file!</p>
-      
-   </body>
-</html>
