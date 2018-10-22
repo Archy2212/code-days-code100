29745dba7b9e7543b03e5da302c28d7f8196fdd2
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-10-22 23:22:26 +0530

    Update pointer2.c

--- c,c++/pointer2.c
+++ c,c++/pointer2.c
@@ -4,6 +4,6 @@ int number=50;
 int *p;      
 p=&number;//stores the address of number variable    
 printf("Address of p variable is %x \n ",p);    
-printf("Value of p variable is %d \n",*p);    
+printf("Value of p variable is %d \n ",*p);    
 return 0;  
 }    
