53bf82304d90fc01a1eb3ff873416e17e899b289
Archy2212 <40794153+Archy2212@users.noreply.github.com>
2018-12-08 00:06:32 +0530

    Update cel_to_fah.py

--- python/cel_to_fah.py
+++ python/cel_to_fah.py
@@ -3,6 +3,6 @@
 # change this value for a different result
 celsius = 37.5
 
-# calculate fahrenheit
+#calculate fahrenheit
 fahrenheit = (celsius * 1.8) + 32
 print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
